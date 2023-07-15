b01cd0f4ef037503ed27d32f054294563bb05e5e
DrPuc <63993644+RealDrPuc@users.noreply.github.com>
2023-07-14 23:27:18 -0300

    Move the data definition to sat_data.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,87 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod sat_data;
+use sat_data::convert_satellite_data_to_json;
+
 use serial2::SerialPort;
 use std::path::PathBuf;
 use std::thread;
 use std::str;
 use std::sync::Arc;
-use serde::{Serialize, Deserialize};
-use serde_json::{json, Value};
+use serde::Serialize;
 
-#[derive(Serialize, Deserialize)]
-struct SensorData {
-    mq4: bool,
-    mq7: bool,
-    mq135: bool,
-    latitude: f64,
-    longitude: f64,
-    altitude: f64,
-    direction: f64,
-    speed: f64,
-    bmp_temperature: f64,
-    pressure: u32,
-    dht_temperature: f64,
-    humidity: f64,
-    w: f64,
-    x: f64,
-    y: f64,
-    z: f64,
-}
-
-#[derive(Clone, serde::Serialize)]
+#[derive(Clone, Serialize)]
 struct Payload {
   message: String,
-}
-
-fn convert_sensor_data_to_json(input: &str) -> Result<Value, serde_json::Error> {
-    let mut sensor_data: SensorData = SensorData {
-        mq4: false,
-        mq7: false,
-        mq135: false,
-        latitude: 0.0,
-        longitude: 0.0,
-        altitude: 0.0,
-        direction: 0.0,
-        speed: 0.0,
-        bmp_temperature: 0.0,
-        pressure: 0,
-        dht_temperature: 0.0,
-        humidity: 0.0,
-        w: 2.0,
-        x: 2.0,
-        y: 2.0,
-        z: 2.0,
-    };
-
-    for entry in input.trim().split(';') {
-        let parts: Vec<&str> = entry.split(':').collect();
-        if parts.len() != 2 {
-            continue;
-        }
-        match parts[0] {
-            "MQ4" => sensor_data.mq4 = parts[1].parse().unwrap_or(false),
-            "MQ7" => sensor_data.mq7 = parts[1].parse().unwrap_or(false),
-            "MQ135" => sensor_data.mq135 = parts[1].parse().unwrap_or(false),
-            "LAT" => sensor_data.latitude = parts[1].parse().unwrap_or(0.0),
-            "LON" => sensor_data.longitude = parts[1].parse().unwrap_or(0.0),
-            "ALT" => sensor_data.altitude = parts[1].parse().unwrap_or(0.0),
-            "DIR" => sensor_data.direction = parts[1].parse().unwrap_or(0.0),
-            "SPD" => sensor_data.speed = parts[1].parse().unwrap_or(0.0),
-            "B-T" => sensor_data.bmp_temperature = parts[1].parse().unwrap_or(0.0),
-            "P" => sensor_data.pressure = parts[1].parse().unwrap_or(0),
-            "D-T" => sensor_data.dht_temperature = parts[1].parse().unwrap_or(0.0),
-            "H" => sensor_data.humidity = parts[1].parse().unwrap_or(0.0),
-            "W" => sensor_data.w = parts[1].parse().unwrap_or(2.0),
-            "X" => sensor_data.x = parts[1].parse().unwrap_or(2.0),
-            "Y" => sensor_data.y = parts[1].parse().unwrap_or(2.0),
-            "Z" => sensor_data.z = parts[1].parse().unwrap_or(2.0),
-            _ => (),
-        }
-    }
-
-    let json_data = json!(sensor_data);
-    Ok(json_data)
 }
 
 fn get_available_ports() -> String {
@@ -119,7 +51,7 @@ fn main() {
                         println!("{:?}", str::from_utf8(&buffer));
                         let sensor_data: String = str::from_utf8(&buffer).unwrap().trim_matches(char::from(0)).to_string();
                         buffer = [0; 192];
-                        let json_sensor_data = convert_sensor_data_to_json(&sensor_data).unwrap();
+                        let json_sensor_data = convert_satellite_data_to_json(&sensor_data).unwrap();
                         let reply: Payload = Payload {
                             message: json_sensor_data.to_string(),
                         };
---
+++ src-tauri/src/sat_data.rs
@@ -0,0 +1,72 @@
+use serde::{Serialize, Deserialize};
+use serde_json::{json, Value};
+
+#[derive(Serialize, Deserialize)]
+struct SatelliteData {
+    mq4: bool,
+    mq7: bool,
+    mq135: bool,
+    latitude: f64,
+    longitude: f64,
+    altitude: f64,
+    direction: f64,
+    speed: f64,
+    bmp_temperature: f64,
+    pressure: u32,
+    dht_temperature: f64,
+    humidity: f64,
+    w: f64,
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+pub fn convert_satellite_data_to_json(input: &str) -> Result<Value, serde_json::Error> {
+    let mut sat_data: SatelliteData = SatelliteData {
+        mq4: false,
+        mq7: false,
+        mq135: false,
+        latitude: 0.0,
+        longitude: 0.0,
+        altitude: 0.0,
+        direction: 0.0,
+        speed: 0.0,
+        bmp_temperature: 0.0,
+        pressure: 0,
+        dht_temperature: 0.0,
+        humidity: 0.0,
+        w: 2.0,
+        x: 2.0,
+        y: 2.0,
+        z: 2.0,
+    };
+
+    for entry in input.trim().split(';') {
+        let parts: Vec<&str> = entry.split(':').collect();
+        if parts.len() != 2 {
+            continue;
+        }
+        match parts[0] {
+            "MQ4" => sat_data.mq4 = parts[1].parse().unwrap_or(false),
+            "MQ7" => sat_data.mq7 = parts[1].parse().unwrap_or(false),
+            "MQ135" => sat_data.mq135 = parts[1].parse().unwrap_or(false),
+            "LAT" => sat_data.latitude = parts[1].parse().unwrap_or(0.0),
+            "LON" => sat_data.longitude = parts[1].parse().unwrap_or(0.0),
+            "ALT" => sat_data.altitude = parts[1].parse().unwrap_or(0.0),
+            "DIR" => sat_data.direction = parts[1].parse().unwrap_or(0.0),
+            "SPD" => sat_data.speed = parts[1].parse().unwrap_or(0.0),
+            "B-T" => sat_data.bmp_temperature = parts[1].parse().unwrap_or(0.0),
+            "P" => sat_data.pressure = parts[1].parse().unwrap_or(0),
+            "D-T" => sat_data.dht_temperature = parts[1].parse().unwrap_or(0.0),
+            "H" => sat_data.humidity = parts[1].parse().unwrap_or(0.0),
+            "W" => sat_data.w = parts[1].parse().unwrap_or(2.0),
+            "X" => sat_data.x = parts[1].parse().unwrap_or(2.0),
+            "Y" => sat_data.y = parts[1].parse().unwrap_or(2.0),
+            "Z" => sat_data.z = parts[1].parse().unwrap_or(2.0),
+            _ => (),
+        }
+    }
+
+    let json_data = json!(sat_data);
+    Ok(json_data)
+}
